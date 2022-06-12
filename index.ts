11d5868898dace71b2444c375f30cfb8e691ac1e
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-06-12 19:56:32 -0300

    add db setup

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
-// import express from "express";
-const express = require("express");
+import express from "express";
+import { PostRouter } from "./src/routes/post.route";
+import { UserRouter } from "./src/routes/user.route";
+// const express = require("express");
 
 class Server {
-  public app = (express.Application = express());
+  public app = (express.application = express());
   private port = 3030;
 
   constructor() {
     this.app.use(express.json());
-    this.app.use("/test", (req: any, res: any)=>{
-        console.log("recived request")
-    })
+
+    // this.app.use("/test", (req: any, res: any) => {
+    //   res.status(200).json({
+    //     message: "recived",
+    //   });
+    // });
+
+    this.app.use("/api", this.Routes());
 
     this.Listen();
   }
@@ -18,6 +25,10 @@ class Server {
     this.app.listen(this.port, () => {
       console.log(`server on in ${this.port}`);
     });
+  }
+
+  Routes(): Array<express.Router> {
+    return [new UserRouter().router, new PostRouter().router];
   }
 }
 
