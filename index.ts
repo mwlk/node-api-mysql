770e7083dd24c8b321b9d7c24dd5b66423defb25
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-06-12 17:33:55 -0300

    add tsc config

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("hello world")
+// import express from "express";
+const express = require("express");
+
+class Server {
+  public app = (express.Application = express());
+  private port = 3030;
+
+  constructor() {
+    this.app.use(express.json());
+    this.app.use("/test", (req: any, res: any)=>{
+        console.log("recived request")
+    })
+
+    this.Listen();
+  }
+
+  Listen() {
+    this.app.listen(this.port, () => {
+      console.log(`server on in ${this.port}`);
+    });
+  }
+}
+
+new Server();
